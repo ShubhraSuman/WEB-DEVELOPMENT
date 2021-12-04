c509c633166bdd159c9eaa33bc31ede076e8a4a0
Shubhra suman <53232818+ShubhraSuman@users.noreply.github.com>
2021-12-04 23:31:28 +0530

    Create Math Object.js

---
+++ JavaScript/js_sandbox/Math Object.js
@@ -0,0 +1,15 @@
+// --------------MATH OBJECT-------------
+
+let val;
+val = Math.PI; // 3.14
+val = Math.E; //2.71
+val = Math.round(2.4); //2
+val = Math.ceil(2.4); //3
+val = Math.sqrt(64); //8
+val = Math.abs(-3); //3
+val = Math.pow(8,2); //64
+val = Math.min(2,9,0,8,7); //0
+console.log(val);
+val = Math.random(); //Gives a random integer
+val = Math.random() * 20; //To get a number from range 0,19
+
