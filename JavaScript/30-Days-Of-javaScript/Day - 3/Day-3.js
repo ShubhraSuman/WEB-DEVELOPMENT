16d773edb479afff9ce8f7ae33715e6e36ef2a01
Shubhra suman <53232818+ShubhraSuman@users.noreply.github.com>
2022-10-19 16:29:41 +0530

    Create Day-3.js

---
+++ JavaScript/30-Days-Of-javaScript/Day - 3/Day-3.js
@@ -0,0 +1,27 @@
+//Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+let firstName = "Shubhra";
+let lastName = "Suman";
+let country = "India";
+let city = "ranchi";
+let age = 20;
+let isMarried = false;
+const now = new Date();
+let year = now.getFullYear();
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(year));
+
+
+Output:
+string
+string
+string
+string
+number
+boolean
+number
