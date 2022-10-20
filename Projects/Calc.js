15d2016744927bbba1d3de9528b10a1ca7f2d910
Shubhra suman <53232818+ShubhraSuman@users.noreply.github.com>
2022-10-20 10:48:26 +0530

    Create Calc.js

---
+++ Projects/Calc.js
@@ -0,0 +1,12 @@
+function display(val){
+  document.getElementById('result').value+= val;
+  return val;
+}
+function clearScreen(){ 
+  document.getElementById('result').value = '';
+}
+function solve(){
+  let x=document.getElementById('result').value
+  let y = eval(x); document.getElementById('result').value = y;
+  return y;
+}
