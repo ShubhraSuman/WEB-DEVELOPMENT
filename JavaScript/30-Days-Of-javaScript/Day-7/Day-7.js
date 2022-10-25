08df684e8fbb4b3ddddad8352007036c79249ca8
Shubhra suman <53232818+ShubhraSuman@users.noreply.github.com>
2022-10-25 21:18:03 +0530

    Create Day-7.js

---
+++ JavaScript/30-Days-Of-javaScript/Day-7/Day-7.js
@@ -0,0 +1,14 @@
+Q1.// Declare a function fullName and it print out your full name.
+let fullName = (firstName, lastName) =>{
+    return `${firstName} ${lastName}`;
+}
+console.log(fullName("Shubhra","Suman"));
+
+
+Q2.// Declare a function addNumbers and it takes two two parameters and it returns sum
+let sum = (num1,num2)=>{
+    return num1+num2;
+}
+console.log(sum(10,20));
+
+
